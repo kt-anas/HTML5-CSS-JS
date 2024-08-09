8a3a3210a11454f5ec471d02589dc04e95131d33
anas <ktmanas2020@gmail.com>
2024-08-09 10:18:17 +0530

    modules

---
+++ modules/module_1/index.js
@@ -0,0 +1,20 @@
+
+// funtions 
+ 
+ var  name = "nameles";
+
+ function nameFunc(){
+
+     var name1 = "name";
+     console.log(name);
+ 
+
+ }
+
+ nameFunc();// nameles
+ console.log(name1);  // not defined
+
+
+//  var: Function-scoped or globally-scoped variable, can be re-declared and updated.
+// let: Block-scoped variable, can be updated but not re-declared in the same scope.
+// const: Block-scoped variable, cannot be updated or re-declared.
