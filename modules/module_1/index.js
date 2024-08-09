a7523ce2d8575aba0165db9c3dfeb919aa2ac8d6
anas <ktmanas2020@gmail.com>
2024-08-09 12:12:13 +0530

    scop

--- modules/module_1/index.js
+++ modules/module_1/index.js
@@ -1,20 +1,8 @@
 
-// funtions 
- 
- var  name = "nameles";
-
- function nameFunc(){
-
-     var name1 = "name";
-     console.log(name);
- 
-
- }
-
- nameFunc();// nameles
- console.log(name1);  // not defined
 
 
-//  var: Function-scoped or globally-scoped variable, can be re-declared and updated.
-// let: Block-scoped variable, can be updated but not re-declared in the same scope.
-// const: Block-scoped variable, cannot be updated or re-declared.
+// const normalFunction =(...arg) =>{
+//     console.log(arg);
+//   }
+//   normalFunction(1, 2, 3); // Output: [1, 2, 3]
+  
