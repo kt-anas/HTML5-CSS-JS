b839007ddb6becf0d2be0639ab1098d92728818c
anas <ktmanas2020@gmail.com>
2024-08-09 14:50:16 +0530

    methods

---
+++ modules/module_1/JS methods/array.js
@@ -0,0 +1,18 @@
+// javascript array methods 
+
+
+//---- push ----//
+
+// Add elements to the end of an array.
+let arr = [1,2,3,4];
+
+arr.push(5);
+console.log(arr); // [ 1, 2, 3, 4, 5 ]
+
+
+//-------pop-------/
+
+//Removes the last element from an array
+let arr1 = [1,2,3,4];
+arr.pop();
+console.log(arr1);// [ 1, 2, 3, 4 ]
