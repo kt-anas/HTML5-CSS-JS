ee557e0e90cea26a50a93dfacb46c19861c6536e
anas <ktmanas2020@gmail.com>
2024-08-12 09:54:01 +0530

    scop

--- modules/module_1/scop.js
+++ modules/module_1/scop.js
@@ -1,25 +1,24 @@
 
 // Scop in javascriopt
-var  name = "nameles";
+// var  name = "nameles";
 
 
-function nameFunc(){
-    // var name1 = "name";
-    let age = 5;
-    // console.log(name);
+// function nameFunc(){
+//     // var name1 = "name";
+//     let age = 5;
+//     // console.log(name);
    
-}
+// }
 
 
-{
+// {
     
-   var name = "na";
-//    console.log(age);
-}
+//    var name = "na";
+// //    console.log(age);
+// }
 
-console.log(name)
 // console.log(name)
-
+// console.log(name)
 
 //    nameFunc();  // nameles
 
@@ -29,3 +28,15 @@ console.log(name)
 //  var: Function-scoped or globally-scoped variable, can be re-declared and updated.
 // let: Block-scoped variable, can be updated but not re-declared in the same scope.
 // const: Block-scoped variable, cannot be updated or re-declared.
+
+
+function a(){
+    let x = 10;
+     return function b(){
+        console.log(x);
+     }
+    
+}
+
+const c = a();
+c();
